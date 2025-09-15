f9b6f4d4218427ba4bf51e4369fb75ba501401f0
G-SURENDRA <surendragara999@gmail.com>
2025-09-15 21:43:55 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummerizer.pipeline.stage01_data_ingestion import DataIngestionPipeline
 from textSummerizer.logging import logger
 from textSummerizer.pipeline.stage02_data_validation import DataValidationPipeline
+from textSummerizer.pipeline.stage03_data_transformation import DataTransformationPipeline
 
 
 STAGE_NAME="Data Ingestion"
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>>>stage {STAGE_NAME} started >>>>>>>>>>>>>>")
     data_validation=DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>stage {STAGE_NAME} completed \n\n>>>>>>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Data Transformation"
+try:
+    logger.info(f">>>>>>>stage {STAGE_NAME} started >>>>>>>>>>>>>>")
+    data_transformation=DataTransformationPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>stage {STAGE_NAME} completed \n\n>>>>>>>>>>>>>>")
 except Exception as e:
     logger.exception(e)

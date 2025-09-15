9fd87d86a8b820fd9081b491ae05c39c17d549cb
G-SURENDRA <surendragara999@gmail.com>
2025-09-15 20:59:55 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummerizer.pipeline.stage01_data_ingestion import DataIngestionPipeline
 from textSummerizer.logging import logger
+from textSummerizer.pipeline.stage02_data_validation import DataValidationPipeline
 
 
 STAGE_NAME="Data Ingestion"
@@ -7,6 +8,18 @@ try:
     logger.info(f">>>>>>>stage {STAGE_NAME} started >>>>>>>>>>>>>>")
     data_ingestion=DataIngestionPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>stage {STAGE_NAME} completed \n\n>>>>>>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME="Data Validation"
+try:
+    logger.info(f">>>>>>>stage {STAGE_NAME} started >>>>>>>>>>>>>>")
+    data_validation=DataValidationPipeline()
+    data_validation.main()
     logger.info(f">>>>>>>stage {STAGE_NAME} completed \n\n>>>>>>>>>>>>>>")
 except Exception as e:
     logger.exception(e)
--- src/textSummerizer/config/configuration.py
+++ src/textSummerizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummerizer.constants import *
 from textSummerizer.utils.common import read_yaml,create_directories
-from textSummerizer.entity.config_entity import DataIngestionConfig
+from textSummerizer.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -27,4 +27,17 @@ class ConfigurationManager:
           )
 
           return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_required_FILES=config.ALL_required_FILES,
+        )
+
+        return data_validation_config
     
